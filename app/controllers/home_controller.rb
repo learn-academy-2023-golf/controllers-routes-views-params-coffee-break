a31d8e98eefbf7043903273dd822e2a86df1e585
TheLavitz <lavitz@gmail.com>
2023-11-27 14:31:25 -0800

    challenges complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,34 @@ class HomeController < ApplicationController
     
     def markayel
     end
+
+    def cubed
+        @number = params[:number]
+    end
+
+    def evenly
+        @num1 = params[:num1]
+        @num2 = params[:num2]
+        if params[:num1].to_i % params[:num2].to_i == 0
+            @result = "Yes"
+        else
+            @result = "No"
+        end
+    end
+
+    def palindrome
+        @word = params[:word]
+        if params[:word].downcase == params[:word].downcase.reverse
+            @palindrome = "Yes"
+        else
+            @palindrome = "No"
+        end
+    end
+
+    def madlib
+        @noun = params[:noun]
+        @verb = params[:verb]
+        @adjective = params[:adjective]
+        @adverb = params[:adverb]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ get '/landing' => 'home#landing'
 root 'home#landing'
 get '/corey' => 'home#corey'
 get '/markayel' => 'home#markayel'
+get '/cubed/:number' => 'home#cubed'
+get '/evenly/:num1/:num2' => 'home#evenly'
+get '/palindrome/:word' => 'home#palindrome'
+get '/madlib/:adjective/:noun/:adverb/:verb' => 'home#madlib'
 end
